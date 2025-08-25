363cbbee670cc4ea5f38a237c7a2a89ed8d8fbad
projectweb-app <colemotorsdomain@gmail.com>
2025-08-24 20:28:47 -0700

    Update API URL to live AWS server

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import UploadComponent from './components/UploadComponent';
 import ResultsComponent from './components/ResultsComponent';
 import AdsenseComponent from './components/AdsenseComponent'; // 1. IMPORT
 
-const API_URL = '/api';
+const API_URL = 'http://13.61.10.85:5000/api'; // <-- USE YOUR AWS PUBLIC IP ADDRESS!
 // 2. GET THE PUBLISHER ID FROM THE .ENV FILE
 const ADSENSE_PUBLISHER_ID = import.meta.env.VITE_ADSENSE_PUBLISHER_ID;
 
